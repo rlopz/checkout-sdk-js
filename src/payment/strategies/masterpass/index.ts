1a3791ede25c931e7da3f1dcaef6c0b11df032c2
Christian Medinilla <christian.medinilla@bigcommerce.com>
2018-10-18 22:47:59 -0500

    feat(checkout): INT-951 Remove useless line in index

--- src/payment/strategies/masterpass/index.ts
+++ src/payment/strategies/masterpass/index.ts
@@ -1,5 +1,4 @@
 export * from './masterpass';
-export { Masterpass, MasterpassHostWindow, MasterpassCheckoutOptions, MasterpassCheckoutCallback } from './masterpass';
 export { default as MasterpassScriptLoader } from './masterpass-script-loader';
 export { default as MasterpassPaymentStrategy } from './masterpass-payment-strategy';
 export { MasterpassPaymentInitializeOptions } from './masterpass-payment-initialize-options';
